060d7b5e18318f257d7bb29cc8dc1be062da2a68
John Seong <wonmor@gmail.com>
2023-11-07 16:13:30 -0800

    Added files

---
+++ Scripts/badcode.m
@@ -0,0 +1,12 @@
+% badcode example in class 11/7/23
+vec = ones(1,4); % preallocating a vector
+for j = 1:4
+    vec(j) = j
+end
+
+% function stub is a placeholder function with only getters, typically
+% used in debugging
+% function [x, y] = getvales
+% x = 33;
+% y = 11;
+% end
---
+++ Scripts/placeholder.m
@@ -0,0 +1,9 @@
+[a, b] = placeholder;
+
+add = a + b;
+
+% local function placeholder
+function [x, y] = placeholder
+x = 9;
+y = 8;
+end % end placeholder function
